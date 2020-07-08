7c29858e538e23987d5952902649f89f3d779f3b
Dionardo Marques <dionardogianluca@gmail.com>
2020-07-07 23:46:27 -0300

    Franquia Service comunicando corretamente

--- resources/js/Service/franquia_service.js
+++ resources/js/Service/franquia_service.js
@@ -8,11 +8,9 @@ class FranquiaAPIService {
         xhttp.onreadystatechange = function () {
             if (this.readyState === 4) {
                 if(this.status === 200) {
-                    //Chama o método sucesso definido no carregarMarcas() do controller
                     ok(JSON.parse(this.responseText));
                 }
                 else {
-                    //Chama o método trataErro definido no carregarMarcas() do controller
                     erro(this.status);
                 }
             }
