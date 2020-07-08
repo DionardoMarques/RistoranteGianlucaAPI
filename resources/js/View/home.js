279a115401dade2f495f570fcde428673b6b62ed
Dionardo Marques <dionardogianluca@gmail.com>
2020-07-07 23:35:23 -0300

    Página home carregando

--- resources/js/View/home.js
+++ resources/js/View/home.js
@@ -21,7 +21,7 @@ class HomeVisual {
         <!------------------------- SERVIÇOS ------------------------->
         <main class="servicos container">
             <article class="servico radius">
-                <a href="cardapio.html"><img src="../img/servicos.jpg" alt="Pratos da casa"></a>
+                <a href="cardapio.html"><img src="resources/img/servicos.jpg" alt="Pratos da casa"></a>
                 <div class="inner">
                     <a href="cardapio.html">Pratos da Casa</a>
                     <h4>Massas, lasanhas e risotos.</h4>
@@ -29,7 +29,7 @@ class HomeVisual {
                 </div>
             </article>
             <article class="servico radius">
-                <a href="cardapio.html"><img src="../img/tele_entrega.jpg" alt="Tele entrega de comida"></a>
+                <a href="cardapio.html"><img src="resources/img/tele_entrega.jpg" alt="Tele entrega de comida"></a>
                 <div class="inner">
                     <a href="cardapio.html">Tele-entrega</a>
                     <h4>Os melhores pratos italianos no conforto da sua casa.</h4>
@@ -37,7 +37,7 @@ class HomeVisual {
                 </div>
             </article>
             <article class="servico radius">
-                <a href="contato.html"><img src="../img/eventos.jpg" alt="Eventos comemorativos com buffet"></a>
+                <a href="contato.html"><img src="resources/img/eventos.jpg" alt="Eventos comemorativos com buffet"></a>
                 <div class="inner">
                     <a href="contato.html">Eventos</a>
                     <h4>Comemore conosco as suas datas especiais.</h4>
@@ -47,16 +47,17 @@ class HomeVisual {
         </main>
         <!------------------------- HORÁRIO DE FUNCIONAMENTO ------------------------->
         <section class="horario container bg-green">
-            <h2> Horário de funcionamento <br> <i class="far fa-clock"></i></h2>
-            <h3> 
-                <em>Segunda a sexta:</em> das 11h às 15h e das 18h30 às 22h45<br>
-                <em>Sábados, domingos e feriados:</em> das 11h às 15h30 e das 18h30 às 22h45
-            </h3>
-            <h3>
-                Mais informações em: (51) 3259-1229 <i class="fab fa-whatsapp"></i><br>
-                *Telefone válido apenas para a cidade de Porto Alegre
-            </h3>
+                <h2> Horário de funcionamento <br> <i class="far fa-clock"></i></h2>
+                <h3> 
+                    <em>Segunda a sexta:</em> das 11h às 15h e das 18h30 às 22h45<br>
+                    <em>Sábados, domingos e feriados:</em> das 11h às 15h30 e das 18h30 às 22h45
+                </h3>
+                <h3>
+                    Mais informações em: (51) 3259-1229 <i class="fab fa-whatsapp"></i><br>
+                    *Telefone válido apenas para a cidade de Porto Alegre
+                </h3>
         </section>
+        </main>
         `;
 
         var home = document.querySelector(this.seletor);
